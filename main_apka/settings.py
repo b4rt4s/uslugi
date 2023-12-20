d47e6d28fb15b48a5b3851bf6200f9f8c6b0de87
b4rt4s <109885481+b4rt4s@users.noreply.github.com>
2023-12-20 06:56:04 +0100

    Update settings.py

--- main_apka/settings.py
+++ main_apka/settings.py
@@ -97,7 +97,7 @@ ASGI_APPLICATION = 'main_apka.asgi.application'
 CACHES = {
     'default': {
         'BACKEND': 'django_redis.cache.RedisCache',
-        'LOCATION': 'redis://127.0.0.1:6379/1',
+        'LOCATION': 'redis://0.0.0.0:6379/1',
         # Use 'redis://redis:6379/1' if 'redis' is the name in a Docker compose file or similar setup
         'OPTIONS': {
             'CLIENT_CLASS': 'django_redis.client.DefaultClient',
@@ -109,7 +109,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": [('127.0.0.1', 6379)],
+            "hosts": [('0.0.0.0', 6379)],
             # Use [("redis", 6379)] if 'redis' is the name in a Docker compose file or similar setup
         },
     },
